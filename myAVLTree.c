3ad5d14d28aba5b56547368f725c724de5f7a511
shubham1010 <shubhamjagdhane1010@gmail.com>
2020-03-02 23:45:43 +0530

    just insertion without balancing height

--- myAVLTree.c
+++ myAVLTree.c
@@ -10,10 +10,35 @@ typedef struct myAVLTree {
 void inorder(AVL *root);
 void insert(AVL **root,int data);
 int height(AVL *node);
+int max(int a,int b);
+void deleteTree(AVL **root);
 
 int main(void) {
-	AVL *head=NULL;
+	AVL *root=NULL;
+	int data;
+	while(1) {
+		printf("\nEnter data for a node: ");
+		scanf("%d",&data);
 
+		if(data==-1)
+			break;
+
+		insert(&root,data);
+	}
+	inorder(root);
+	deleteTree(&root);
+}
+
+void deleteTree(AVL **root) {
+	if(*root) {
+		deleteTree(&((*root)->left));
+		deleteTree(&((*root)->right));
+		free(*root);
+	}
+}
+
+int max(int a,int b) {
+	return (a>b?a:b);
 }
 
 int height(AVL *node) {
@@ -31,10 +56,10 @@ void insert(AVL **root,int data) {
 		*root=newNode;
 	}
 	else if (data < (*root)->data) {
-		insert((*root)->left,data);
+		insert(&((*root)->left),data);
 	}
 	else {
-		insert((*root)->right,data);
+		insert(&((*root)->right),data);
 	}
 
 	(*root)->height = max(height((*root)->left),height((*root)->right))+1;
@@ -43,7 +68,7 @@ void insert(AVL **root,int data) {
 void inorder(AVL *root) {
 	if(root) {
 		inorder(root->left);
-		printf("\n%d with height %d",root->data);
+		printf("\n%d with height %d",root->data,root->height);
 		inorder(root->right);
 	}
 }
