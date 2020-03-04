964a1bd9445e73f3964a0ab97a2eb701ac110bdf
shubham1010 <shubhamjagdhane1010@gmail.com>
2020-03-04 23:53:44 +0530

    deletion function

--- myAVLTree.c
+++ myAVLTree.c
@@ -16,15 +16,17 @@ AVL *rightRotate(AVL *X);
 int getBalanceFactor(AVL *node);
 AVL *newNode(int data);
 void deleteTree(AVL **root);
+AVL *deleteNode(AVL *root, int data);
+AVL  *minValueNode(AVL *node);
 
 int main(void) {
 	AVL *root=NULL;
-	int data;
+	int data,ch;
 	while(1) {
-		printf("\n1: Insert\n2: Delete\n3: Exit");
+		printf("\n1: Insert\n2: Delete\n3: Inorder\n4: Exit");
 		printf("\nEnter your choice: ");
 		scanf("%d",&ch);
-	 	if(ch==3)
+	 	if(ch==4)
 	 		break;
 
 		switch(ch) {
@@ -33,12 +35,17 @@ int main(void) {
 				printf("\nEnter data for a node: ");
 				scanf("%d",&data);
 			 	root=insert(root,data);
-				break
+				break;
 			case 2:
 				printf("\nEnter data to delete node: ");
 				scanf("%d",&data);
-				root = delete(root,data);
+				root = deleteNode(root,data);
 				break;
+			case 3:
+				printf("\nInorder: ");
+				inorder(root);
+				break;
+		}
 	}
 	inorder(root);
 	deleteTree(&root);
@@ -145,4 +152,96 @@ void inorder(AVL *root) {
 		printf("\n%d with height %d",root->data,root->height);
 		inorder(root->right);
 	}
+}
+
+AVL  *minValueNode(AVL *node) {
+    AVL *current = node; 
+  
+    /* loop down to find the leftmost leaf */
+    while (current->left != NULL) 
+        current = current->left; 
+  
+    return current; 
+} 
+
+AVL *deleteNode(AVL *root, int data) { 
+    // STEP 1: PERFORM STANDARD BST DELETE 
+  
+    if (root == NULL) 
+        return root; 
+  
+    // If the data to be deleted is smaller than the 
+    // root's data, then it lies in left subtree 
+    if ( data < root->data ) 
+        root->left = deleteNode(root->left, data); 
+  
+    // If the data to be deleted is greater than the 
+    // root's data, then it lies in right subtree 
+    else if( data > root->data ) 
+        root->right = deleteNode(root->right, data); 
+  
+    // if data is same as root's data, then This is 
+    // the node to be deleted 
+    else { 
+        // node with only one child or no child 
+        if( (root->left == NULL) || (root->right == NULL) ) { 
+            AVL *temp = root->left ? root->left : root->right; 
+  
+            // No child case 
+            if (temp == NULL) { 
+                temp = root; 
+                root = NULL; 
+            } 
+            else 			// One child case 
+				*root = *temp; // Copy the contents of the non-empty child 
+				free(temp); 
+        } 
+        else { 
+            // node with two children: Get the inorder 
+            // successor (smallest in the right subtree) 
+            AVL *temp = minValueNode(root->right); 
+  
+            // Copy the inorder successor's data to this node 
+            root->data = temp->data; 
+  
+            // Delete the inorder successor 
+            root->right = deleteNode(root->right, temp->data); 
+        } 
+    } 
+  
+    // If the tree had only one node then return 
+    if (root == NULL) 
+      return root; 
+  
+    // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE 
+    root->height = 1 + max(height(root->left), 
+                           height(root->right)); 
+  
+    // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to 
+    // check whether this node became unbalanced) 
+    int balance = getBalanceFactor(root); 
+  
+    // If this node becomes unbalanced, then there are 4 cases 
+  
+    // Left Left Case 
+    if (balance > 1 && getBalanceFactor(root->left) >= 0) 
+        return rightRotate(root); 
+  
+    // Left Right Case 
+    if (balance > 1 && getBalanceFactor(root->left) < 0) { 
+        root->left =  leftRotate(root->left); 
+        return rightRotate(root); 
+    } 
+  
+    // Right Right Case 
+    if (balance < -1 && getBalanceFactor(root->right) <= 0) 
+        return leftRotate(root); 
+  
+    // Right Left Case 
+    if (balance < -1 && getBalanceFactor(root->right) > 0) { 
+        root->right = rightRotate(root->right); 
+        return leftRotate(root); 
+    } 
+  
+    return root; 
 }
